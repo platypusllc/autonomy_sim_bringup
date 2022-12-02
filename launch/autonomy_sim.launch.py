9ee1ad01e6ab91ba0a5a7bf05c4f040693c41cd4
Chris Tomaszewski <chris@senseplatypus.com>
2022-12-02 16:41:38 -0500

    Updated launch file to launch task manager and mission manager.

--- launch/autonomy_sim.launch.py
+++ launch/autonomy_sim.launch.py
@@ -11,6 +11,16 @@ def generate_launch_description():
         package="simusv",
         executable="boatsim"
     )
+    task_node = Node(
+        package="autonomy",
+        executable="taskman"
+    )
+    mission_node = Node(
+        package="autonomy",
+        executable="missionman"
+    )
     ld.add_action(cop_node)
     ld.add_action(sim_node)
+    ld.add_action(task_node)
+    ld.add_action(mission_node)
     return ld
